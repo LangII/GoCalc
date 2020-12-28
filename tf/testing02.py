aa376e4d9ffffcc90743f2c3484c45642ea3ada5
David Lang <langdjii@gmail.com>
2020-12-28 10:44:58 -0600

    built pred_black_angles w/ tf.where for speed

--- tf/testing02.py
+++ tf/testing02.py
@@ -24,13 +24,13 @@ NO_STONE_VALUE = 0
 BLACK_STONE_VALUE = +1
 WHITE_STONE_VALUE = -1
 
-BOARD = tf.constant([
-    [ 0,  0,  0,  0],
-    [ 0, -1, +1,  0],
-    [-1,  0,  0, -1],
-    [ 0, +1, +1,  0],
-], dtype='int32')
-print(""), print(BOARD)
+# BOARD = tf.constant([
+#     [ 0,  0,  0,  0],
+#     [ 0,  0, +1,  0],
+#     [ 0, +1,  0, +1],
+#     [ 0,  0,  0,  0],
+# ], dtype='int32')
+# print("\nBOARD =", BOARD)
 
 # BOARD = tf.constant([
 #     [ 0,  0,  0,  0,  0,  0,  0,  0,  0],
@@ -43,30 +43,30 @@ print(""), print(BOARD)
 #     [ 0,  0,  0,  0,  0,  0,  0,  0,  0],
 #     [ 0,  0,  0,  0,  0,  0,  0,  0,  0]
 # ], dtype='int32')
-# print(""), print(BOARD)
+# print("\nBOARD =", BOARD)
 
-# BOARD = tf.constant([
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0, +1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, +1,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, +1,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, +1,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-#     [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
-# ], dtype='int32')
-# print(""), print(BOARD)
+BOARD = tf.constant([
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0, +1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, +1,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, +1,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, +1,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+    [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
+], dtype='int32')
+print("\nBOARD =", BOARD)
 
 ####################################################################################################$
 
@@ -74,7 +74,7 @@ print(""), print(BOARD)
 
 """ In case I need an empty board at a later point. """
 # empty_board = tf.zeros(BOARD.shape, dtype='int32')
-# print(""), print(empty_board)
+# print("\nempty_board =", empty_board)
 
 
 
@@ -83,74 +83,96 @@ the model. (as well as empty_coords_count) """
 
 empty_coords = tf.cast(tf.where(tf.equal(BOARD, 0)), dtype='int32')
 empty_coords_count = empty_coords.shape[0]
-# print(""), print(empty_coords)
+# print("\nempty_coords =", empty_coords)
 
 
 
-""" next_moves is a list of all possible next moves.  Where each "next move" is represented by a
+""" pred_moves is a list of all possible next moves.  Where each "next move" is represented by a
 full board position. """
 
 def getNextMoves(coord):
-    next_move = tf.SparseTensor([coord], [BLACK_STONE_VALUE], BOARD.shape)
-    return tf.sparse.to_dense(next_move)
-next_moves = tf.vectorized_map(fn=lambda coord: getNextMoves(coord), elems=empty_coords)
+    pred_move = tf.SparseTensor([coord], [BLACK_STONE_VALUE], BOARD.shape)
+    return tf.sparse.to_dense(pred_move)
+pred_moves = tf.vectorized_map(fn=lambda coord: getNextMoves(coord), elems=empty_coords)
 tiled_board = tf.tile(tf.reshape(BOARD, [1, *BOARD.shape]), [empty_coords_count, 1, 1])
-next_moves = tiled_board + next_moves
-# print(""), print(next_moves)
+pred_moves = tiled_board + pred_moves
+# print("\npred_moves =", pred_moves)
 
 
 
-""" next_empty_coords is a tensor very similar to empty_coords.  Except what empty_coords does for
-the current board position, next_empty_coords does for every possible board position in
-next_moves. """
+""" pred_empty_coords is a tensor very similar to empty_coords.  Except what empty_coords does for
+the current board position, pred_empty_coords does for every possible board position in
+pred_moves. """
 
-next_empty_coords = tf.cast(tf.where(tf.equal(next_moves, NO_STONE_VALUE)), dtype='int32')
-next_empty_coords = tf.reshape(next_empty_coords[:, 1:], [empty_coords_count, -1, 2])
-# print(""), print(next_empty_coords)
-
-
-
-""" next_black_coords is very similar to next_empty_coords except what next_empty_coords does for
-positions on board with value of 0, next_black_coords does for positions on board with value of 1. """
-
-next_black_coords = tf.cast(tf.where(tf.equal(next_moves, BLACK_STONE_VALUE)), dtype='int32')
-next_black_coords = tf.reshape(next_black_coords[:, 1:], [empty_coords_count, -1, 2])
-# print(""), print(next_black_coords)
+pred_empty_coords = tf.cast(tf.where(tf.equal(pred_moves, NO_STONE_VALUE)), dtype='int32')
+pred_empty_coords = tf.reshape(pred_empty_coords[:, 1:], [empty_coords_count, -1, 2])
+# print("\npred_empty_coords =", pred_empty_coords)
 
 
 
-""" next_empty_tiled takes the values from next_empty_coords and tiles them for calculating
-next_black_dist and next_white_dist. """
+""" pred_black_coords is very similar to pred_empty_coords except what pred_empty_coords does for
+positions on board with value of 0, pred_black_coords does for positions on board with value of 1. """
 
-next_black_size = next_black_coords.shape[1]
-next_empty_coords_shape = next_empty_coords.get_shape().as_list()
-next_empty_coords_shape.insert(2, 1)
-next_empty_coords = tf.reshape(next_empty_coords, next_empty_coords_shape)
-next_empty_tiled = tf.tile(next_empty_coords, tf.constant([1, 1, next_black_size, 1]))
-# print(""), print(next_empty_tiled)
+pred_black_coords = tf.cast(tf.where(tf.equal(pred_moves, BLACK_STONE_VALUE)), dtype='int32')
+pred_black_coords = tf.reshape(pred_black_coords[:, 1:], [empty_coords_count, -1, 2])
+# print("\npred_black_coords =", pred_black_coords)
 
 
 
-""" next_black_tiled takes the values from next_black_coords and tiles them for calculating
-next_black_dist. """
+""" pred_empty_tiled takes the values from pred_empty_coords and tiles them for calculating
+pred_black_dist and pred_white_dist. """
 
-next_empty_size = next_empty_coords.shape[1]
-next_black_coords_shape = next_black_coords.get_shape().as_list()
-next_black_coords_shape.insert(1, 1)
-next_black_coords = tf.reshape(next_black_coords, next_black_coords_shape)
-next_black_tiled = tf.tile(next_black_coords, tf.constant([1, next_empty_size, 1, 1]))
-# print(""), print(next_black_tiled)
+pred_black_size = pred_black_coords.shape[1]
+pred_empty_coords_shape = pred_empty_coords.get_shape().as_list()
+pred_empty_coords_shape.insert(2, 1)
+pred_empty_coords = tf.reshape(pred_empty_coords, pred_empty_coords_shape)
+pred_empty_tiled = tf.tile(pred_empty_coords, tf.constant([1, 1, pred_black_size, 1]))
+# print("\npred_empty_tiled =", pred_empty_tiled)
 
 
 
-""" next_black_dist is a complex tensor.  The outer most dimension (D1) represents board positions
+""" pred_black_tiled takes the values from pred_black_coords and tiles them for calculating
+pred_black_dist. """
+
+pred_empty_size = pred_empty_coords.shape[1]
+pred_black_coords_shape = pred_black_coords.get_shape().as_list()
+pred_black_coords_shape.insert(1, 1)
+pred_black_coords = tf.reshape(pred_black_coords, pred_black_coords_shape)
+pred_black_tiled = tf.tile(pred_black_coords, tf.constant([1, pred_empty_size, 1, 1]))
+# print("\npred_black_tiled =", pred_black_tiled)
+
+
+
+PRED_BLACK_NORMALS = pred_black_tiled - pred_empty_tiled
+
+""" pred_black_dist is a complex tensor.  The outer most dimension (D1) represents board positions
 for every possible move that could come next.  The next inner dimension (D2) represents every
 empty position on the board (for each board of (D1)).  The next inner dimension (D3) represents
 every black stone on the board.  The lowest dimension (D4) contains the calculated values of the
 distance between every empty position on the board (D2) and every position on the board with a black
 stone (D3) (for every possible next move (D1)).  """
-next_norms = next_empty_tiled - next_black_tiled
-flat_next_norms = tf.reshape(tf.cast(next_norms, dtype='float32'), [-1, 2])
-next_black_dist = tf.norm(flat_next_norms, ord='euclidean', axis=1)
-next_black_dist = tf.reshape(next_black_dist, next_norms.shape[:-1])
-print(""), print(next_black_dist)
+
+flat_pred_normals = tf.reshape(tf.cast(PRED_BLACK_NORMALS, dtype='float32'), [-1, 2])
+pred_black_dist = tf.norm(flat_pred_normals, ord='euclidean', axis=1)
+pred_black_dist = tf.reshape(pred_black_dist, PRED_BLACK_NORMALS.shape[:-1])
+# print("\npred_black_dist =", pred_black_dist)
+
+
+
+""" pred_black_angles is a complex tensor (very similar to pred_black_dist).  All dimensions
+are representative to the same values as the dimensions of pred_black_dist.  Except the lowest
+dimension (D4).  The values in D4 represent the angle from an empty position (origin) on the board
+to a position with a black stone (coordinate). """
+
+pred_normals_y = tf.cast(PRED_BLACK_NORMALS[:, :, :, 0] * -1, dtype='float32')
+pred_normals_x = tf.cast(PRED_BLACK_NORMALS[:, :, :, 1], dtype='float32')
+pred_black_angles = tf.atan2(pred_normals_y, pred_normals_x) * (180 / math.pi)
+pred_black_angles = tf.where(pred_black_angles > 0, pred_black_angles, pred_black_angles + 360)
+# print("\pred_black_angles =", pred_black_angles)
+
+
+
+pred_move = 0
+print("\npred_moves =", pred_moves[pred_move])
+print("\npred_black_dist =", pred_black_dist[pred_move])
+print("\npred_black_angles =", pred_black_angles[pred_move])
