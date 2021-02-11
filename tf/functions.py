4a409593610b4a7fb3097237d964c77c6f968574
David Lang <langdjii@gmail.com>
2021-02-11 16:09:21 -0600

    added getCountOfValue, getPosOfValue, reshapeFlatten, replaceValueAtIndex

--- tf/functions.py
+++ tf/functions.py
@@ -100,6 +100,21 @@ def getCount(t, v):
 
 
 
+def getCountOfValue(t, v):
+    return int(tf.reduce_sum(tf.cast(tf.equal(t, v), dtype='int32')).numpy())
+
+
+
+def getPosOfValue(t, v):
+    return tf.cast(tf.where(tf.equal(t, v)), dtype=t.dtype)
+
+
+
+def reshapeFlatten(t):
+    return tf.reshape(t, [-1])
+
+
+
 def reshapeInsertDim(t, dim):
     reshape = t.shape.as_list()
     reshape.insert(dim, 1)
@@ -119,6 +134,12 @@ def reshapeMergeDims(t, dims):
     del reshape[dims[0]:(dims[1] + 1)]
     reshape.insert(dims[0], -1)
     return tf.reshape(t, reshape)
+
+
+
+def replaceValueAtIndex(t, v, i):
+    bool_mask = tf.sparse.to_dense(tf.SparseTensor([i], [True], t.shape))
+    return tf.cast(tf.where(bool_mask, v, t), dtype=t.dtype)
 
 
 
