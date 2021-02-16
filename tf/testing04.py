c639e79ad83837cad98696e0705e44e4451a45ca
David Lang <langdjii@gmail.com>
2021-02-15 21:45:47 -0600

    built group assignment

--- tf/testing04.py
+++ tf/testing04.py
@@ -5,8 +5,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 
 from functions import (
-    reshapeFlatten, reshapeInsertDim, reshapeAddDim, getCountOfValue, getPosOfValue,
-    replaceValueAtIndex
+    reshapeFlatten, reshapeInsertDim, reshapeAddDim, reshapeMergeDims, getCountOfValue,
+    getPosOfValue, replaceValueAtIndex
 )
 
 ####################################################################################################
@@ -40,21 +40,7 @@ BOARD_SIZE = BOARD.shape.as_list()[0]
 
 def main():
 
-    # inp = tf.constant([0, 1, 2, 3])
-    inp = tf.range(0, 100000)
-
-    outp = tf.TensorArray(dtype='int32', size=0, dynamic_size=True, clear_after_read=False)
-    outp = outp.write(0, inp[0])
-
-    cond = lambda i, _: i < inp.shape[0]
-    body = lambda i, outp: (i + 1, outp.write(i, inp[i] + outp.read(i - 1)))
-    _, outp = tf.while_loop(cond, body, (1, outp))
-    outp = outp.stack()
-
-    print(inp)
-    print(outp)
-
-    exit()
+    # exit()
 
     # flat_board = reshapeFlatten(BOARD)
     # print(flat_board, "<- flat_board\n")
@@ -88,34 +74,118 @@ def getGroups(stone_count, stone_pos):
     def getNeighbors(pos):
         y, x = pos[0] + 1, pos[1] + 1
         neighbors = tf.stack([
-            temp_board[y][x],
-            temp_board[y - 1][x],
-            temp_board[y][x + 1],
-            temp_board[y + 1][x],
-            temp_board[y][x - 1],
+            [temp_board[y][x], temp_board[y - 1][x]],
+            [temp_board[y][x], temp_board[y][x + 1]],
+            [temp_board[y][x], temp_board[y + 1][x]],
+            [temp_board[y][x], temp_board[y][x - 1]],
         ])
         return neighbors
     neighbors = tf.map_fn(
         fn=lambda elem: getNeighbors(elem),
         elems=stone_pos,
     )
-    # neighbors = tf.sort(neighbors, direction='DESCENDING')
-    # neighbors = tf.sort(neighbors)
+    neighbors = reshapeMergeDims(neighbors, [0, 1])
+    neighbors_mask = tf.equal(tf.reduce_any(tf.equal(neighbors, 0), axis=1), False)
+    neighbors = tf.boolean_mask(neighbors, neighbors_mask)
     print(neighbors, "<- neighbors\n")
 
-    temp_temp_value = reshapeAddDim(reshapeInsertDim(temp_value, 1))
-    n_compare_this = tf.cast(tf.tile(temp_temp_value, [1, stone_count, 5]), dtype=BOARD.dtype)
-    # print(n_compare_this, "<- n_compare_this\n")
+    group_assign = tf.TensorArray(
+        dtype=BOARD.dtype, size=0, dynamic_size=True, clear_after_read=False
+    )
+    _, group_assign = tf.while_loop(
+        lambda i, _:  i < stone_count,
+        lambda i, group_assign:  (i + 1, group_assign.write(i, i + 1)),
+        (0, group_assign)
+    )
 
-    n_compare_to_that = tf.tile(reshapeInsertDim(neighbors, 0), [stone_count, 1, 1])
-    # print(n_compare_to_that, "<- n_compare_to_that\n")
+    def setGroupAssign(i, group_assign):
+        a = neighbors[i, 0] - 1
+        b = neighbors[i, 1] - 1
+        group_assign = group_assign.write(b, group_assign.read(a))
+        return i + 1, group_assign
+    _, group_assign = tf.while_loop(
+        lambda i, _:  i < neighbors.shape[0],
+        lambda i, group_assign:  setGroupAssign(i, group_assign),
+        (0, group_assign)
+    )
 
-    compare_bool_mask = tf.equal(n_compare_this, n_compare_to_that)
+    group_assign = group_assign.stack()
+    print(group_assign, "<- group_assign\n")
+
+    """
+    # inp = tf.constant([0, 1, 2, 3])
+    inp = tf.range(0, 1000)
+
+    outp = tf.TensorArray(dtype='int32', size=0, dynamic_size=True, clear_after_read=False)
+    outp = outp.write(0, inp[0])
+
+    cond = lambda i, _: i < inp.shape[0]
+    body = lambda i, outp: (i + 1, outp.write(i, inp[i] + outp.read(i - 1)))
+    _, outp = tf.while_loop(cond, body, (1, outp))
+    outp = outp.stack()
+
+    print(inp)
+    print(outp)
+    """
+
+    # """ get single_neighbors """
+    # single_neighbors = tf.TensorArray(
+    #     dtype=BOARD.dtype, size=0, dynamic_size=True, clear_after_read=False
+    # )
+    # def setSingleNeighbors(i, single_neighbors):
+    #     pos = stone_pos[i]
+    #     y, x = pos[0] + 1, pos[1] + 1
+    # single_neighbors = tf.while_loop(
+    #     lambda i, _: i < stone_count,
+    #     lambda i, single_neighbors:  setSingleNeighbors(i, single_neighbors),
+    #     (0, single_neighbors)
+    # )
+    #
+    # print(single_neighbors )
+
+    # while_out = tf.TensorArray(dtype=BOARD.dtype, size=0, dynamic_size=True, clear_after_read=False)
+    # _, while_out = tf.while_loop(
+    #     lambda i, _: i < stone_count,
+    #     lambda i, while_out: (i + 1, while_out.write(i, i + 1)),
+    #     (0, while_out)
+    # )
+    #
+    # def whileFunc(i, while_out):
+    #     pos = stone_pos[i]
+    #     group_value = while_out.read(i)
+    #
+    #
+    #
+    #     # while_out = while_out.write(i, group_value)
+    #
+    #     return (i + 1, while_out)
+    #
+    # _, while_out = tf.while_loop(
+    #     lambda i, _: i < stone_count, # cond
+    #     lambda i, while_out: whileFunc(i, while_out), # body
+    #     (0, while_out) # loop_vars
+    # )
+    # while_out = while_out.stack()
+
+    # print(while_out, "<- while_out\n")
+
+
+
+
+
+
+    # temp_temp_value = reshapeAddDim(reshapeInsertDim(temp_value, 1))
+    # n_compare_this = tf.cast(tf.tile(temp_temp_value, [1, stone_count, 5]), dtype=BOARD.dtype)
+    # # print(n_compare_this, "<- n_compare_this\n")
+    #
+    # n_compare_to_that = tf.tile(reshapeInsertDim(neighbors, 0), [stone_count, 1, 1])
+    # # print(n_compare_to_that, "<- n_compare_to_that\n")
+    #
+    # compare_bool_mask = tf.equal(n_compare_this, n_compare_to_that)
+    # # print(compare_bool_mask, "<- compare_bool_mask\n")
+    # compare_bool_mask = tf.reduce_any(compare_bool_mask, axis=2)
+    # compare_bool_mask = reshapeAddDim(compare_bool_mask)
     # print(compare_bool_mask, "<- compare_bool_mask\n")
-    compare_bool_mask = tf.reduce_any(compare_bool_mask, axis=2)
-    compare_bool_mask = reshapeAddDim(compare_bool_mask)
-    print(compare_bool_mask, "<- compare_bool_mask\n")
-
 
     return
 
